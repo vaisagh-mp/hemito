5d4ff86ddb4f6268264dc08c854ecc516f3768e4
PontusDigimark <contact@pontusdigi.com>
2025-05-23 09:21:19 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,188 @@
+// Slick Slider - Our Clients
+$(function () {
+  $(".center").slick({
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: { slidesToShow: 4 },
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 3 },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          centerMode: false,
+        },
+      },
+    ],
+  });
+});
+
+// Navigation Overlay
+function openNav() {
+  var nav = document.getElementById("myNav");
+  nav.style.width = "100%";
+  nav.style.height = "100%";
+}
+
+function closeNav() {
+  var nav = document.getElementById("myNav");
+  nav.style.width = "0%";
+  nav.style.height = "0%";
+}
+
+// Mobile Menu Toggle
+const menu = document.querySelector(".menu");
+const menuMain = menu.querySelector(".menu-main");
+const goBack = menu.querySelector(".go-back");
+const menuTrigger = document.querySelector(".mobile-menu-trigger");
+const closeMenu = menu.querySelector(".mobile-menu-close");
+let subMenu;
+
+menuMain.addEventListener("click", (e) => {
+  if (!menu.classList.contains("active")) return;
+  if (e.target.closest(".menu-item-has-children")) {
+    const hasChildren = e.target.closest(".menu-item-has-children");
+    showSubMenu(hasChildren);
+  }
+});
+
+goBack.addEventListener("click", () => {
+  hideSubMenu();
+});
+
+menuTrigger.addEventListener("click", toggleMenu);
+closeMenu.addEventListener("click", toggleMenu);
+document.querySelector(".menu-overlay").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  menu.classList.toggle("active");
+  document.querySelector(".menu-overlay").classList.toggle("active");
+}
+
+function showSubMenu(hasChildren) {
+  subMenu = hasChildren.querySelector(".sub-menu");
+  subMenu.classList.add("active");
+  subMenu.style.animation = "slideLeft 0.5s ease forwards";
+  const menuTitle =
+    hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
+  menu.querySelector(".current-menu-title").innerHTML = menuTitle;
+  menu.querySelector(".mobile-menu-head").classList.add("active");
+}
+
+function hideSubMenu() {
+  subMenu.style.animation = "slideRight 0.5s ease forwards";
+  setTimeout(() => {
+    subMenu.classList.remove("active");
+  }, 300);
+  menu.querySelector(".current-menu-title").innerHTML = "";
+  menu.querySelector(".mobile-menu-head").classList.remove("active");
+}
+
+window.onresize = function () {
+  if (this.innerWidth > 991 && menu.classList.contains("active")) {
+    toggleMenu();
+  }
+};
+
+// Client Testimonial Slider - Swiper
+const swiper = new Swiper(".client-testimonial-slider", {
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// Hero Carousel
+{
+  class SliderClip {
+    constructor(el) {
+      this.el = el;
+      this.Slides = Array.from(this.el.querySelectorAll("li"));
+      this.Nav = Array.from(this.el.querySelectorAll("aside a"));
+      this.totalSlides = this.Slides.length;
+      this.current = 0;
+      this.autoPlay = true; //true or false
+      this.timeTrans = 4000; //transition time in milliseconds
+      this.IndexElements = [];
+
+      for (let i = 0; i < this.totalSlides; i++) {
+        this.IndexElements.push(i);
+      }
+
+      this.setCurret();
+      this.initEvents();
+    }
+    setCurret() {
+      this.Slides[this.current].classList.add("current");
+      this.Nav[this.current].classList.add("current_dot");
+    }
+    initEvents() {
+      const self = this;
+
+      this.Nav.forEach((dot) => {
+        dot.addEventListener("click", (ele) => {
+          ele.preventDefault();
+          this.changeSlide(this.Nav.indexOf(dot));
+        });
+      });
+
+      this.el.addEventListener("mouseenter", () => (self.autoPlay = false));
+      this.el.addEventListener("mouseleave", () => (self.autoPlay = true));
+
+      setInterval(function () {
+        if (self.autoPlay) {
+          self.current =
+            self.current < self.Slides.length - 1 ? self.current + 1 : 0;
+          self.changeSlide(self.current);
+        }
+      }, this.timeTrans);
+    }
+    changeSlide(index) {
+      this.Nav.forEach((allDot) => allDot.classList.remove("current_dot"));
+
+      this.Slides.forEach((allSlides) =>
+        allSlides.classList.remove("prev", "current")
+      );
+
+      const getAllPrev = (value) => value < index;
+
+      const prevElements = this.IndexElements.filter(getAllPrev);
+
+      prevElements.forEach((indexPrevEle) =>
+        this.Slides[indexPrevEle].classList.add("prev")
+      );
+
+      this.Slides[index].classList.add("current");
+      this.Nav[index].classList.add("current_dot");
+    }
+  }
+
+  const slider = new SliderClip(document.querySelector(".slider"));
+}
